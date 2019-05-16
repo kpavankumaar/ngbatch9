f21137a5cde0be76ec32f42ac5bfb9380a583bd5
nbitsmeanstack49 <nbitsmeanstack49@gmail.com>
2019-05-16 10:13:27 +0530

    cross field validation and custom validation

--- angular/basics/src/app/reactiveform/reactiveform.component.ts
+++ angular/basics/src/app/reactiveform/reactiveform.component.ts
@@ -1,6 +1,26 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, Validators, AbstractControl } from '@angular/forms';
+import { ValueTransformer } from '@angular/compiler/src/util';
+function ratingRange(min:number,max:number){
+  return function rating(c:AbstractControl):{[key:string]:boolean} | null{
+    if(c.value <= max && c.value >= min ){
+      return {
+        match:true
+      }
+    }
+    return null
+  }
+}
 
+// function phoneValidation(c:AbstractControl): {[key:string]:boolean} | null{
+//   let phone = c.value
+//   if(phone.length !== 10){
+//     return {
+//       match:true
+//     }
+//   }
+//   return null
+// }
 @Component({
   selector: 'app-reactiveform',
   templateUrl: './reactiveform.component.html',
@@ -12,8 +32,21 @@ export class ReactiveformComponent implements OnInit {
   showHideAddress: boolean = false;
   ngOnInit() {
     this.customerForm = new FormGroup({
-      email:new FormControl(''),
-      password:new FormControl()
+      firstName:new FormControl('', Validators.required),
+      lastName:new FormControl(''),
+      rating:new FormControl('',[Validators.required,ratingRange(0,5)]),
+      // phone:new FormControl('',[Validators.required,Validators.minLength(10),Validators.maxLength(10)]),
+      // phone:new FormControl('',[Validators.required,phoneValidation]),
+      phone:new FormControl(''),
+      email:new FormControl('',[Validators.required]),
+      notifications: new FormControl(),
+      address: new FormGroup({
+        hno:new FormControl(),
+        street: new FormControl(),
+        city: new FormControl(),
+        state: new FormControl(),
+        zipcode: new FormControl()
+      })
     })
     console.log(this.customerForm);
   }
@@ -23,5 +56,17 @@ export class ReactiveformComponent implements OnInit {
   showAddress(){
     this.showHideAddress = this.showHideAddress == false ? true : false;
   }
+  notificationMessage(e){
+    console.log(e);
+    let phone = this.customerForm.get('phone');
+
+    if( e === 'phone'){
+      phone.setValidators(Validators.required);
+
+    }else{
+      phone.clearValidators();
+    }
+    phone.updateValueAndValidity()
+  } 
 
 }
