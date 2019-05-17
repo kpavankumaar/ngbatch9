513b97fd29a7c51c7e7e1d457548c080053992ab
nbitsmeanstack49 <nbitsmeanstack49@gmail.com>
2019-05-17 09:36:48 +0530

    completed form validations , custom form validation , cross field validation , Form builder and FormGroup

--- angular/basics/src/app/reactiveform/reactiveform.component.ts
+++ angular/basics/src/app/reactiveform/reactiveform.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators, AbstractControl } from '@angular/forms';
+import { FormGroup, FormControl, Validators, AbstractControl, FormBuilder } from '@angular/forms';
 import { ValueTransformer } from '@angular/compiler/src/util';
+import { fbind } from 'q';
 function ratingRange(min:number,max:number){
   return function rating(c:AbstractControl):{[key:string]:boolean} | null{
     if(c.value <= max && c.value >= min ){
@@ -12,15 +13,15 @@ function ratingRange(min:number,max:number){
   }
 }
 
-// function phoneValidation(c:AbstractControl): {[key:string]:boolean} | null{
-//   let phone = c.value
-//   if(phone.length !== 10){
-//     return {
-//       match:true
-//     }
-//   }
-//   return null
-// }
+function phoneValidation(c:AbstractControl): {[key:string]:boolean} | null{
+  let phone = c.value
+  if(phone.length !== 10){
+    return {
+      match:true
+    }
+  }
+  return null
+}
 @Component({
   selector: 'app-reactiveform',
   templateUrl: './reactiveform.component.html',
@@ -28,26 +29,43 @@ function ratingRange(min:number,max:number){
 })
 export class ReactiveformComponent implements OnInit {
   customerForm:FormGroup;
-  constructor() { }
+  constructor(private fb:FormBuilder) { }
   showHideAddress: boolean = false;
   ngOnInit() {
-    this.customerForm = new FormGroup({
-      firstName:new FormControl('', Validators.required),
+    this.customerForm = this.fb.group({
+      firstName:this.fb.control(''),
       lastName:new FormControl(''),
       rating:new FormControl('',[Validators.required,ratingRange(0,5)]),
       // phone:new FormControl('',[Validators.required,Validators.minLength(10),Validators.maxLength(10)]),
       // phone:new FormControl('',[Validators.required,phoneValidation]),
-      phone:new FormControl(''),
+      phone:new FormControl('',[Validators.minLength(10),Validators.maxLength(10)]),
       email:new FormControl('',[Validators.required]),
       notifications: new FormControl(),
       address: new FormGroup({
-        hno:new FormControl(),
-        street: new FormControl(),
-        city: new FormControl(),
-        state: new FormControl(),
-        zipcode: new FormControl()
+        hno:new FormControl('', Validators.required),
+        street: new FormControl('', Validators.required),
+        city: new FormControl('', Validators.required),
+        state: new FormControl('', Validators.required),
+        zipcode: new FormControl('', Validators.required)
       })
     })
+    // this.customerForm = new FormGroup({
+    //   firstName:new FormControl('', Validators.required),
+    //   lastName:new FormControl(''),
+    //   rating:new FormControl('',[Validators.required,ratingRange(0,5)]),
+    //   // phone:new FormControl('',[Validators.required,Validators.minLength(10),Validators.maxLength(10)]),
+    //   // phone:new FormControl('',[Validators.required,phoneValidation]),
+    //   phone:new FormControl('',[Validators.minLength(10),Validators.maxLength(10)]),
+    //   email:new FormControl('',[Validators.required]),
+    //   notifications: new FormControl(),
+    //   address: new FormGroup({
+    //     hno:new FormControl('', Validators.required),
+    //     street: new FormControl('', Validators.required),
+    //     city: new FormControl('', Validators.required),
+    //     state: new FormControl('', Validators.required),
+    //     zipcode: new FormControl('', Validators.required)
+    //   })
+    // })
     console.log(this.customerForm);
   }
   submit(){
@@ -59,14 +77,16 @@ export class ReactiveformComponent implements OnInit {
   notificationMessage(e){
     console.log(e);
     let phone = this.customerForm.get('phone');
-
+    let email = this.customerForm.get('email');
     if( e === 'phone'){
-      phone.setValidators(Validators.required);
-
+      phone.setValidators([Validators.required,phoneValidation]);
+      email.clearValidators();
     }else{
       phone.clearValidators();
+      email.setValidators(Validators.required);
     }
-    phone.updateValueAndValidity()
+    phone.updateValueAndValidity();
+    email.updateValueAndValidity();
   } 
 
 }
